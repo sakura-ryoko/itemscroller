461e2e207554a3ff896214fc8abaae3610562abf
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-01-02 04:57:37 -0500

    1.20.x revert

--- src/main/java/fi/dy/masa/itemscroller/recipes/RecipeStorage.java
+++ src/main/java/fi/dy/masa/itemscroller/recipes/RecipeStorage.java
@@ -3,8 +3,14 @@ package fi.dy.masa.itemscroller.recipes;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-
 import javax.annotation.Nonnull;
+
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtTagSizeTracker;
+import net.minecraft.screen.slot.Slot;
 
 import fi.dy.masa.itemscroller.ItemScroller;
 import fi.dy.masa.itemscroller.Reference;
@@ -12,63 +18,70 @@ import fi.dy.masa.itemscroller.config.Configs;
 import fi.dy.masa.itemscroller.util.Constants;
 import fi.dy.masa.malilib.util.FileUtils;
 import fi.dy.masa.malilib.util.StringUtils;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtIo;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.nbt.NbtSizeTracker;
-import net.minecraft.screen.slot.Slot;
 
-public class RecipeStorage {
+public class RecipeStorage
+{
     private static final RecipeStorage INSTANCE = new RecipeStorage(8 * 18);
 
     private final RecipePattern[] recipes;
     private int selected;
     private boolean dirty;
 
-    public static RecipeStorage getInstance() {
+    public static RecipeStorage getInstance()
+    {
         return INSTANCE;
     }
 
-    public RecipeStorage(int recipeCount) {
+    public RecipeStorage(int recipeCount)
+    {
         this.recipes = new RecipePattern[recipeCount];
         this.initRecipes();
     }
 
-    private void initRecipes() {
-        for (int i = 0; i < this.recipes.length; i++) {
+    private void initRecipes()
+    {
+        for (int i = 0; i < this.recipes.length; i++)
+        {
             this.recipes[i] = new RecipePattern();
         }
     }
 
-    public int getSelection() {
+    public int getSelection()
+    {
         return this.selected;
     }
 
-    public void changeSelectedRecipe(int index) {
-        if (index >= 0 && index < this.recipes.length) {
+    public void changeSelectedRecipe(int index)
+    {
+        if (index >= 0 && index < this.recipes.length)
+        {
             this.selected = index;
             this.dirty = true;
         }
     }
 
-    public void scrollSelection(boolean forward) {
+    public void scrollSelection(boolean forward)
+    {
         this.changeSelectedRecipe(this.selected + (forward ? 1 : -1));
     }
 
-    public int getFirstVisibleRecipeId() {
+    public int getFirstVisibleRecipeId()
+    {
         return this.getCurrentRecipePage() * this.getRecipeCountPerPage();
     }
 
-    public int getTotalRecipeCount() {
+    public int getTotalRecipeCount()
+    {
         return this.recipes.length;
     }
 
-    public int getRecipeCountPerPage() {
+    public int getRecipeCountPerPage()
+    {
         return 18;
     }
 
-    public int getCurrentRecipePage() {
+    public int getCurrentRecipePage()
+    {
         return this.getSelection() / this.getRecipeCountPerPage();
     }
 
@@ -77,8 +90,10 @@ public class RecipeStorage {
      * If the index is invalid, then the first recipe is returned, instead of null.
      */
     @Nonnull
-    public RecipePattern getRecipe(int index) {
-        if (index >= 0 && index < this.recipes.length) {
+    public RecipePattern getRecipe(int index)
+    {
+        if (index >= 0 && index < this.recipes.length)
+        {
             return this.recipes[index];
         }
 
@@ -86,42 +101,51 @@ public class RecipeStorage {
     }
 
     @Nonnull
-    public RecipePattern getSelectedRecipe() {
+    public RecipePattern getSelectedRecipe()
+    {
         return this.getRecipe(this.getSelection());
     }
 
-    public void storeCraftingRecipeToCurrentSelection(Slot slot, HandledScreen<?> gui, boolean clearIfEmpty) {
+    public void storeCraftingRecipeToCurrentSelection(Slot slot, HandledScreen<?> gui, boolean clearIfEmpty)
+    {
         this.storeCraftingRecipe(this.getSelection(), slot, gui, clearIfEmpty);
     }
 
-    public void storeCraftingRecipe(int index, Slot slot, HandledScreen<?> gui, boolean clearIfEmpty) {
+    public void storeCraftingRecipe(int index, Slot slot, HandledScreen<?> gui, boolean clearIfEmpty)
+    {
         this.getRecipe(index).storeCraftingRecipe(slot, gui, clearIfEmpty);
         this.dirty = true;
     }
 
-    public void clearRecipe(int index) {
+    public void clearRecipe(int index)
+    {
         this.getRecipe(index).clearRecipe();
         this.dirty = true;
     }
 
-    private void readFromNBT(NbtCompound nbt) {
-        if (nbt == null || nbt.contains("Recipes", Constants.NBT.TAG_LIST) == false) {
+    private void readFromNBT(NbtCompound nbt)
+    {
+        if (nbt == null || nbt.contains("Recipes", Constants.NBT.TAG_LIST) == false)
+        {
             return;
         }
 
-        for (int i = 0; i < this.recipes.length; i++) {
+        for (int i = 0; i < this.recipes.length; i++)
+        {
             this.recipes[i].clearRecipe();
         }
 
         NbtList tagList = nbt.getList("Recipes", Constants.NBT.TAG_COMPOUND);
         int count = tagList.size();
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             NbtCompound tag = tagList.getCompound(i);
 
             int index = tag.getByte("RecipeIndex");
 
-            if (index >= 0 && index < this.recipes.length) {
+            if (index >= 0 && index < this.recipes.length)
+            {
                 this.recipes[index].readFromNBT(tag);
             }
         }
@@ -129,11 +153,14 @@ public class RecipeStorage {
         this.changeSelectedRecipe(nbt.getByte("Selected"));
     }
 
-    private NbtCompound writeToNBT(@Nonnull NbtCompound nbt) {
+    private NbtCompound writeToNBT(@Nonnull NbtCompound nbt)
+    {
         NbtList tagRecipes = new NbtList();
 
-        for (int i = 0; i < this.recipes.length; i++) {
-            if (this.recipes[i].isValid()) {
+        for (int i = 0; i < this.recipes.length; i++)
+        {
+            if (this.recipes[i].isValid())
+            {
                 NbtCompound tag = new NbtCompound();
                 tag.putByte("RecipeIndex", (byte) i);
                 this.recipes[i].writeToNBT(tag);
@@ -147,11 +174,14 @@ public class RecipeStorage {
         return nbt;
     }
 
-    private String getFileName() {
-        if (Configs.Generic.SCROLL_CRAFT_RECIPE_FILE_GLOBAL.getBooleanValue() == false) {
+    private String getFileName()
+    {
+        if (Configs.Generic.SCROLL_CRAFT_RECIPE_FILE_GLOBAL.getBooleanValue() == false)
+        {
             String worldName = StringUtils.getWorldOrServerName();
 
-            if (worldName != null) {
+            if (worldName != null)
+            {
                 return "recipes_" + worldName + ".nbt";
             }
         }
@@ -159,55 +189,69 @@ public class RecipeStorage {
         return "recipes.nbt";
     }
 
-    private File getSaveDir() {
+    private File getSaveDir()
+    {
         return new File(FileUtils.getMinecraftDirectory(), Reference.MOD_ID);
     }
 
-    public void readFromDisk() {
-        try {
+    public void readFromDisk()
+    {
+        try
+        {
             File saveDir = this.getSaveDir();
 
-            if (saveDir != null) {
+            if (saveDir != null)
+            {
                 File file = new File(saveDir, this.getFileName());
 
-                if (file.exists() && file.isFile() && file.canRead()) {
+                if (file.exists() && file.isFile() && file.canRead())
+                {
                     FileInputStream is = new FileInputStream(file);
-                    this.readFromNBT(NbtIo.readCompressed(is, NbtSizeTracker.ofUnlimitedBytes()));
+                    this.readFromNBT(NbtIo.readCompressed(is, NbtTagSizeTracker.ofUnlimitedBytes()));
                     is.close();
-                    // ItemScroller.logger.info("Read recipes from file '{}'", file.getPath());
+                    //ItemScroller.logger.info("Read recipes from file '{}'", file.getPath());
                 }
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             ItemScroller.logger.warn("Failed to read recipes from file", e);
         }
     }
 
-    public void writeToDisk() {
-        if (this.dirty) {
-            try {
+    public void writeToDisk()
+    {
+        if (this.dirty)
+        {
+            try
+            {
                 File saveDir = this.getSaveDir();
 
-                if (saveDir.exists() == false) {
-                    if (saveDir.mkdirs() == false) {
-                        ItemScroller.logger.warn("Failed to create the recipe storage directory '{}'",
-                                saveDir.getPath());
+                if (saveDir.exists() == false)
+                {
+                    if (saveDir.mkdirs() == false)
+                    {
+                        ItemScroller.logger.warn("Failed to create the recipe storage directory '{}'", saveDir.getPath());
                         return;
                     }
                 }
 
-                File fileTmp = new File(saveDir, this.getFileName() + ".tmp");
+                File fileTmp  = new File(saveDir, this.getFileName() + ".tmp");
                 File fileReal = new File(saveDir, this.getFileName());
                 FileOutputStream os = new FileOutputStream(fileTmp);
                 NbtIo.writeCompressed(this.writeToNBT(new NbtCompound()), os);
                 os.close();
 
-                if (fileReal.exists()) {
+                if (fileReal.exists())
+                {
                     fileReal.delete();
                 }
 
                 fileTmp.renameTo(fileReal);
                 this.dirty = false;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 ItemScroller.logger.warn("Failed to write recipes to file!", e);
             }
         }
