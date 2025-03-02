7dcef08e9ff0df31ee7418e68d69e2d3a821a561
Sakura Ryoko <sakura@ryoko.soy>
2025-03-02 16:18:40 -0500

    bump to 25w09b

--- src/main/java/fi/dy/masa/itemscroller/recipes/RecipePattern.java
+++ src/main/java/fi/dy/masa/itemscroller/recipes/RecipePattern.java
@@ -1,12 +1,12 @@
 package fi.dy.masa.itemscroller.recipes;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import com.llamalad7.mixinextras.lib.apache.commons.tuple.Pair;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import com.llamalad7.mixinextras.lib.apache.commons.tuple.Pair;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -33,7 +33,6 @@ import fi.dy.masa.itemscroller.mixin.IMixinClientRecipeBook;
 import fi.dy.masa.itemscroller.mixin.IMixinRecipeBookScreen;
 import fi.dy.masa.itemscroller.mixin.IMixinRecipeBookWidget;
 import fi.dy.masa.itemscroller.recipes.CraftingHandler.SlotRange;
-import fi.dy.masa.itemscroller.util.Constants;
 import fi.dy.masa.itemscroller.util.InventoryUtils;
 
 public class RecipePattern
@@ -410,11 +409,11 @@ public class RecipePattern
 
     public void readFromNBT(@Nonnull NbtCompound nbt, @Nonnull DynamicRegistryManager registryManager)
     {
-        if (nbt.contains("Result", Constants.NBT.TAG_COMPOUND) && nbt.contains("Ingredients", Constants.NBT.TAG_LIST))
+        if (nbt.contains("Result") && nbt.contains("Ingredients"))
         {
-            NbtList tagIngredients = nbt.getList("Ingredients", Constants.NBT.TAG_COMPOUND);
+            NbtList tagIngredients = nbt.getOrCreateList("Ingredients");
             int count = tagIngredients.size();
-            int length = nbt.getInt("Length");
+            int length = nbt.getInt("Length" , -1);
 
             if (length > 0)
             {
@@ -423,8 +422,8 @@ public class RecipePattern
 
             for (int i = 0; i < count; i++)
             {
-                NbtCompound tag = tagIngredients.getCompound(i);
-                int slot = tag.getInt("Slot");
+                NbtCompound tag = tagIngredients.getOrCreateCompound(i);
+                int slot = tag.getInt("Slot", -1);
 
                 if (slot >= 0 && slot < this.recipe.length)
                 {
@@ -432,7 +431,7 @@ public class RecipePattern
                 }
             }
 
-            this.result = fi.dy.masa.malilib.util.InventoryUtils.fromNbtOrEmpty(registryManager, nbt.getCompound("Result"));
+            this.result = fi.dy.masa.malilib.util.InventoryUtils.fromNbtOrEmpty(registryManager, nbt.getOrCreateCompound("Result"));
         }
     }
 
