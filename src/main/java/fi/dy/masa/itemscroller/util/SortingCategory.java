28ee11632d9239193d84ca513aebaa9ae728f64c
Sakura Ryoko <sakura@ryoko.soy>
2026-06-27 09:54:13 -0400

    Check crafting of items with components (Via MaLiLib bump also)

--- src/main/java/fi/dy/masa/itemscroller/util/SortingCategory.java
+++ src/main/java/fi/dy/masa/itemscroller/util/SortingCategory.java
@@ -91,7 +91,8 @@ public class SortingCategory implements IConfigLockedListType
 
         if (id != null)
         {
-            Entry entry = Entry.fromString(id.getPath()); 
+            Entry entry = Entry.fromString(id.getPath());
+
             if (entry != null)
             {
                 return entry;
